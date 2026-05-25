9da47982518627ae8fc821a2bbe94f83c8fa68ed
NerdsAreTheCoolKids <kevindix3008@gmail.com>
2026-05-25 15:10:41 +0200

    Aufgabe 3 update

--- TI2/Aufgabe3_1.s
+++ TI2/Aufgabe3_1.s
@@ -17,7 +17,7 @@
 .equ STATUS_OK, 0
 
 _satAdd10:
-  push {r1-r3, fp, lr}
+  push {r1-r3, lr}
   add r2, r2, r3
   mov r1,r2
   ldr r3, =~0x3ff
@@ -27,16 +27,16 @@ _satAdd10:
   ldrne r0, =ERROR_VALUE_OVERFLOW
 
   moveq r0, #STATUS_OK
-  pop {r1-r3, fp, pc}
+  pop {r1-r3, pc}
 
 _satMul10:
-  push {r2-r4} //12
+  push {r2-r4, lr} // 68
   
   mov r2, #0
   mov r3, #0
 
-  ldrh r2, [sp, #12] // lese 46
-  ldrh r3, [sp, #14] // lese 48
+  ldrh r2, [sp, #22] // lese 42
+  ldrh r3, [sp, #16] // lese 36
 
   mul r4, r2, r3
   mov r3, r4
@@ -44,17 +44,84 @@ _satMul10:
   ands r3, r3, r2
   ldrne r4, =0x3FF
   ldrne r0, =ERROR_VALUE_OVERFLOW
-  strh r4, [sp, #16]
+  strh r4, [sp, #20] // 40
   moveq r0, #STATUS_OK
-  pop {r2-r4}      
+  pop {r2-r4, lr}      
   bx  lr
 
-Pack10:
-  push {r2-r5} // 4 + 4+4+4 =16
+_packScale:
+  push {r3-r7, lr} // 60
+  sub sp, sp, #24  // 36
+  mov r4, #0xFF
+  mov r0, #0
 
-  ldrh r2, [sp, #28]
-  ldrh r3, [sp, #26]
-  ldrh r4, [sp, #24]
+  ldr r7, [sp, #52]
+  str r7, [sp, #8] // 44
+  
+  bl UnPack10 
+  cmp r0, #0
+  bmi _packScaleError
+
+  ldrh r7, [sp, #56] // Faktor zum Skalieren
+  cmp r7, r4
+  bhi _packScaleError
+  
+  strh r7, [sp, #6] // 48
+
+  mov r6, #0
+  mov r7, #2    // Zähler für Schleife
+  mov r3, #8    // Indexe für Register
+
+multLoop:
+  ldrh r4, [sp, r3] 
+  strh r4, [sp, #0] 
+  bl _satMul10 
+  ldrh r5, [sp, #4]
+  strh r5, [sp, r3]
+
+  cmp r7, #0
+  subne r7, r7, #1
+  addne r3, r3, #2
+  bne multLoop
+
+  bl Pack10
+  ldr r6, [sp, #0]
+  str r6, [sp, #48]
+
+_packScaleExit:
+  add sp, sp, #24
+  pop {r3-r7, lr}
+  bx lr
+
+_packScaleError:
+  ldr r0, =ERROR_VALUE_OVERFLOW
+  mov r3, #0
+  str r3, [sp, #48]
+  b _packScaleExit
+
+Pack10_overflow:
+  mov r2, #0
+  str r2, [sp, #20]
+  ldr r0, =ERROR_VALUE_OVERFLOW
+  pop {r2-r5}
+  bx lr
+
+Pack10:
+  push {r1-r5} // 64
+  
+  mov r1, #0xFF
+
+  ldrh r2, [sp, #32] // 48
+  cmp r2, r1
+  bhi Pack10_overflow
+
+  ldrh r3, [sp, #30] // 46
+  cmp r2, r1
+  bhi Pack10_overflow
+
+  ldrh r4, [sp, #28] // 44
+  cmp r2, r1
+  bhi Pack10_overflow
 
   mov r5, #0
   lsl r3, #10
@@ -64,56 +131,70 @@ Pack10:
   orr r5, r5, r3
   orr r5, r5, r4
 
-  str r5, [sp, #16]
-    
-  pop {r2-r5}
-
+  str r5, [sp, #20]
+  pop {r1-r5}
   bx  lr
 
-UnPack10:
-  push {r2-r6, lr} //24
-
+UnPack10_overflow:
   mov r2, #0
+  strh r2, [sp, #36] // schreiben 60
+  strh r2, [sp, #34] // 58
+  strh r2, [sp, #32]
+  ldr r0, =ERROR_VALUE_OVERFLOW
+  pop {r2-r6, lr}
+  bx lr
 
-  ldr r2, [sp, #24]   // lesen bei 56
+UnPack10:
+  push {r2-r6, lr} // 60
+
+  ldr r2, [sp, #32]   // lesen bei 92
+  lsr r2, #30
+  cmp r2, #0
+  bne UnPack10_overflow
+  
   ldr r6, =0x3FF      /*Maske für die ersten 10 Bits*/
 
   mov r3, #0
   mov r4, #0
   mov r5, #0
-
+  
+  ldr r2, [sp, #32]
   and r2, r2, r6
   orr r3, r3, r2
   
-  ldr r2, [sp, #24]
+  ldr r2, [sp, #32]
   lsr r2, #10
   and r2, r2, r6
   orr r4, r4, r2
 
-  ldr r2, [sp, #24]
+  ldr r2, [sp, #32]
   lsr r2, #20
   and r2, r2, r6
   orr r5, r5, r2
 
-  strh r3, [sp, #36] // schreiben 60
-  strh r4, [sp, #34] // 58
-  strh r5, [sp, #30] // 56
+  strh r3, [sp, #36] // schreiben 96
+  strh r4, [sp, #34] // 94
+  strh r5, [sp, #32] // 92
 
   pop {r2-r6, lr}
   bx lr
 
+
 _packMin:
   push {r4-r7, lr} // 20
-  sub sp, sp, #12 // 32
+  sub sp, sp, #16 // 4
 
-  ldr r7, [sp, #40]
-  str r7, [sp, #0]
+  ldr r7, [sp, #44]
+  str r7, [sp, #8]
   
   bl UnPack10
 
-  ldrh r4, [sp, #12]
+  cmp r0, #0
+  bmi _packminError
+
+  ldrh r4, [sp, #8]
   ldrh r5, [sp, #10]
-  ldrh r6, [sp, #8]
+  ldrh r6, [sp, #12]
 
   cmp r4, r5
   movlt r7, r4
@@ -122,23 +203,33 @@ _packMin:
   cmp r7, r6
   movge r7, r6
 
-  strh r7, [sp, #40]
+  strh r7, [sp, #44]
   
+_packminExit:
+  add sp, sp, #16
   pop {r4-r7, lr} 
   bx lr
 
+_packminError:
+  mov r4, #0
+  strh r4, [sp, #44]
+  b _packminExit
+
 _packMax:
-  push {r4-r7, lr}
-  sub sp, sp, #12
+  push {r4-r7, lr} //24
+  sub sp, sp, #24 // -4
   
-  ldr r7, [sp, #36]
+  ldr r7, [sp, #44]
   str r7, [sp, #8]
   
   bl UnPack10
 
-  ldrh r4, [sp, #0]
-  ldrh r5, [sp, #2]
-  ldrh r6, [sp, #4]
+  cmp r0, #0
+  bmi _packMaxError
+
+  ldrh r4, [sp, #12]
+  ldrh r5, [sp, #10]
+  ldrh r6, [sp, #8]
 
   cmp r4, r5
   movge r7, r4
@@ -147,76 +238,55 @@ _packMax:
   cmp r7, r6
   movlt r7, r6
 
-  strh r7, [sp, #32]
+  strh r7, [sp, #44]
   
+
+_packMaxExit:
+  add sp, sp, #24
   pop {r4-r7, lr}
   bx lr
-
-_packScale:
-  push {r3-r7, lr}
-  sub sp, sp, #12
-
-  ldr r7, [sp, #40]
-  str r7, [sp, #8] // 56
   
-  bl UnPack10  
-
-  ldrh r7, [sp, #48] // Faktor zum Skalieren
-
+_packMaxError:
   mov r4, #0
-  mov r6, #0
-  mov r7, #2    // Zähler für Schleife
-  mov r3, #0    // Indexe für Register
+  strh r4, [sp, #44]
+  b _packMaxExit
 
-multLoop:
-  ldrh r4, [sp, r3] 
-  strh r4, [sp, #0] // Parameter setzen für satMul
-  ldrh r5, [sp, #48] 
-  strh r5, [sp, #2] // Parameter setzen für satMul
-
-  bl _satMul10 
-
-  ldrh r5, [sp, #8]
-  orr r6, r6, r5
-
-  cmp r7, #0
-  subne r7, r7, #1
-  lslne r6, #10
-  addne r3, r3, #2
-  bne multLoop
   
-  str r6, [sp,#36]
-
-  pop {r3-r7, lr}
-  bx lr
-
 _packRange:
-  push {r4-r7, lr}
-  sub sp, sp, #12
+  push {r4-r7, lr} // 64
+  sub sp, sp, #24 // 40
 
-  ldr r7, [sp, #36] 
-  str r7, [sp, #4] 
+  ldr r7, [sp, #44]  
+  str r7, [sp, #8] 
   
   bl _packMin
 
   ldrh r6, [sp, #8]
 
-  ldr r7, [sp, #36]
-  str r7, [sp, #4]
+  ldr r7, [sp, #44]
+  str r7, [sp, #0]
 
   bl _packMax
 
-  ldrh r7, [sp, #8]
+  ldrh r7, [sp, #0]
 
   sub r5, r7, r6
 
-  strh r5, [sp, #28]
+  strh r5, [sp, #44]
   
+_packRangeExit:
+  add sp, sp, #24
   pop {r4-r7, lr}
   bx lr
 
+_packRangeError:
+  mov r4, #0
+  strh r4, [sp, #28]
+  b _packRangeExit
+
+
 main:
-    sub sp, sp, #16
+    sub sp, sp, #16 //84
     
     ldr r3, =#0x3FF
     ldr r2, =#0x3FF
@@ -227,7 +297,7 @@ main:
     mov r2, #3
 
     strh r3, [sp, #0]
-    strh r2, [sp, #2]
+    strh r2, [sp, #6]
     bl _satMul10
     ldrh r1, [sp, #4]
   
@@ -241,8 +311,8 @@ main:
 
     bl Pack10
 
-    mov r1, #0
     ldr r1, [sp, #0]
+    str r1, [sp, #8]
       
     bl UnPack10
 
@@ -250,9 +320,9 @@ main:
     ldrh r3, [sp, #10]
     ldrh r4, [sp, #8]
 
-    ldr r2, =0xF
+    ldr r2, =0xFE
     ldr r3, =0xE
-    ldr r4, =0xF
+    ldr r4, =0xFF
 
     lsl r2, #10
     orr r2, r2, r3
@@ -262,7 +332,6 @@ main:
     str r2, [sp, #8] 
 
     bl _packMin
-
 
     mov r2, #0
     ldrh r2, [sp, #8]
@@ -276,26 +345,24 @@ main:
     lsl r2, #10
     orr r2, r2, r4 
 
-    str r2, [sp, #4]
-    mov r2, #0
-    str r2, [sp, #8] 
+    str r2, [sp, #0]
 
     bl _packMax
 
-    ldrh r2, [sp, #8]
+    ldrh r2, [sp, #0]
 
-    ldr r2, =0x3FF
-    ldr r3, =0x3AF
-    ldr r4, =0xC0C
+    ldr r2, =0xA
+    ldr r3, =0xA
+    ldr r4, =0XA
 
     lsl r2, #10
     orr r2, r2, r3
     lsl r2, #10
     orr r2, r2, r4 
 
-    str r2, [sp, #4]
+    str r2, [sp, #4] /* Gepackte Zahl zum skalieren bei 88 */
     mov r2, #3
-    strh r2, [sp, #8] 
+    strh r2, [sp, #8] /* Faktor bei 92 */
 
     bl _packScale
 
@@ -309,13 +376,11 @@ main:
     lsl r2, #10
     orr r2, r2, r4 
 
-    str r2, [sp, #4]
-    mov r2, #0
-    str r2, [sp, #8] 
+    str r2, [sp, #0]
 
     bl _packRange
 
-    ldrh r2, [sp, #8]
+    ldrh r2, [sp, #0]
 
 
 stop:
